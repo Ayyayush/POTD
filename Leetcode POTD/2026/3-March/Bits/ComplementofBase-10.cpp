a185b9b5896cb8e0d0a62450082fe473d77663f1
Ayyayush <treddytalks@gmail.com>
2026-03-17 21:43:50 +0530

    March-17

--- Leetcode POTD/2026/3-March/Bits/ComplementofBase-10.cpp
+++ Leetcode POTD/2026/3-March/Bits/ComplementofBase-10.cpp
@@ -1,18 +1,93 @@
 class Solution {
 public:
     int bitwiseComplement(int n) {
+        if (n == 0) return 1;   // edge case
 
-        if(n == 0) return 1;                         // special case: complement of 0 is 1
-        
-        int mask = 0;
-        int temp = n;
+        string s = "";
 
-        while(temp > 0)
-        {
-            mask = (mask << 1) | 1;                  // creating mask with all 1s equal to number of bits in n
-            temp >>= 1;
+        // ! Convert to BINARY (fix: use %2 not %10)
+        while (n) {
+            int rem = n % 2;
+            s += to_string(rem);
+            n /= 2;
         }
 
-        return n ^ mask;                             // XOR with mask flips only significant bits
+        reverse(s.begin(), s.end());   // ! fix order
+
+        // ! flip bits
+        for (int i = 0; i < s.size(); i++) {
+            s[i] = (s[i] == '0') ? '1' : '0';
+        }
+
+        // ! convert binary string to decimal
+        int sum = 0;
+        for (int i = 0; i < s.size(); i++) {
+            sum = sum * 2 + (s[i] - '0');
+        }
+
+        return sum;
     }
 };
+
+
+
+
+/******************************************************* C++ *******************************************************/
+//Approach-1
+//T.C : O(log(n))
+//S.C : O(1)
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if(n == 0)
+            return 1;
+        
+        int result = 0;
+        int counter = 0;
+        while(n) {
+            int r = n%2;
+            result += (pow(2,counter)*!r);
+            counter++;
+            n = n>>1;
+        }
+        return result;
+    }
+};
+
+//Approach-2
+//T.C : O(log(n))
+//S.C : O(1)
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if (n == 0)
+            return 1;
+
+        int mask = 1;
+
+        while (mask < n) {
+            mask = (mask << 1) | 1;  // builds 111...1
+        }
+        
+        return n ^ mask;
+    }
+};
+
+
+//Approach-3
+//T.C : O(1)
+//S.C : O(1)
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if (n == 0)
+            return 1;
+
+        int bits = floor(log2(n)) + 1;  // number of bits
+        int mask = (1 << bits) - 1;     // 2^bits - 1 = 111...1
+
+        return n ^ mask;
+    }
+};
+
+
